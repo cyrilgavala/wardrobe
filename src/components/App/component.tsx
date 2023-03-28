8c07cea293ac96c7b7ca6ac90c23bf88e17be80e
Cyril Gavala <39592035+cyrilgavala@users.noreply.github.com>
2023-03-28 20:21:32 +0200

    Initial commit

---
+++ src/components/App/component.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.css";
+
+export const App = () => (
+  <div className="App">
+    <p>This is a template for React app with typescript, es-lint and prettier.</p>
+  </div>
+);
